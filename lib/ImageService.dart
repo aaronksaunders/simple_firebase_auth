ee376d33c824f10b502710da8d50763615c45b01
aaron k saunders <aaronksaunders@gmail.com>
2019-07-02 09:55:51 -0400

    created a seperate image service

--- lib/ImageService.dart
+++ lib/ImageService.dart
@@ -1,53 +1,68 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:flutter/cupertino.dart';
 
-class AuthService with ChangeNotifier {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+ 
 
-  ///
-  /// return the Future with firebase user object FirebaseUser if one exists
-  ///
-  Future<FirebaseUser> getUser() {
-    return _auth.currentUser();
-  }
+class ImageService with ChangeNotifier {
+ 
+  uploadTheFile(Map<String,File> _imageInfo, Function _updateCallback(StorageTaskSnapshot _progress)) async {
+    var downloadURL;
 
-  // wrapping the firebase calls
-  Future logout() async {
-    var result = FirebaseAuth.instance.signOut();
-    notifyListeners();
-    return result;
-  }
+    // get current user
+    var currentUser = await FirebaseAuth.instance.currentUser();
+    var uniqueStr =
+        DateTime.now().millisecondsSinceEpoch.toString() + currentUser.uid;
 
-  // wrapping the firebase calls
-  Future createUser(
-      {String firstName,
-      String lastName,
-      String email,
-      String password}) async {
-    var u = await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(email: email, password: password);
+    // convert thumb to base64 string to save with info for faster
+    // screen updates
+    List<int> imageBytes = await _imageInfo['imageThumb'].readAsBytes();
+    String base64Image = base64Encode(imageBytes);
 
-    UserUpdateInfo info = UserUpdateInfo();
-    info.displayName = '$firstName $lastName';
-    return await u.updateProfile(info);
-  }
+    // upload to storage image to storage
+    final StorageReference ref = FirebaseStorage.instance
+        .ref()
+        .child('images-' + currentUser.uid)
+        .child(uniqueStr);
 
-  ///
-  /// wrapping the firebase call to signInWithEmailAndPassword
-  /// `email` String
-  /// `password` String
-  ///
-  Future<FirebaseUser> loginUser({String email, String password}) async {
-    try {
-      var result = await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: password);
-      // since something changed, let's notify the listeners...
-      notifyListeners();
-      return result;
-    } catch (e) {
-      throw new AuthException(e.code, e.message);
-    }
+    final StorageUploadTask uploadTask = ref.putFile(
+      _imageInfo['image'],
+      StorageMetadata(
+        contentLanguage: 'en',
+        //customMetadata: <String, String>{'activity': 'test'},
+      ),
+    );
+
+    uploadTask.events.listen((_event) async {
+      print(_event);
+      if (_event.type == StorageTaskEventType.progress) {
+        print(
+            _event.snapshot.bytesTransferred / _event.snapshot.totalByteCount);
+        _updateCallback(_event.snapshot);
+      } else if (_event.type == StorageTaskEventType.success) {
+        print(_event.snapshot.ref);
+        downloadURL = await _event.snapshot.ref.getDownloadURL();
+
+        // upload to image collection
+        var result = await Firestore.instance.collection('Images').add({
+          'image': downloadURL,
+          'thumb': base64Image,
+          'owner_id': currentUser.uid,
+          'owner': currentUser.email,
+          'subject': "test image upload " + uniqueStr
+        });
+      } else if (_event.type == StorageTaskEventType.failure) {
+        print(_event.snapshot.error);
+      }
+    }, onDone: () async {
+      print("onDone");
+    }, onError: (_error) {
+      print(_error);
+    });
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,10 @@
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'ImageService.dart';
 import 'auth.dart';
 import 'components/FileUploadButtonBar.dart';
 import 'components/ImageList.dart';
@@ -23,61 +21,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   File imageThumb;
   File image;
-
-  uploadTheFile(Function _updateCallback(StorageTaskSnapshot _progress)) async {
-    var downloadURL;
-
-    // get current user
-    var currentUser = await FirebaseAuth.instance.currentUser();
-    var uniqueStr =
-        DateTime.now().millisecondsSinceEpoch.toString() + currentUser.uid;
-
-    // convert thumb to base64 string to save with info for faster
-    // screen updates
-    List<int> imageBytes = await imageThumb.readAsBytes();
-    String base64Image = base64Encode(imageBytes);
-
-    // upload to storage image to storage
-    final StorageReference ref = FirebaseStorage.instance
-        .ref()
-        .child('images-' + currentUser.uid)
-        .child(uniqueStr);
-
-    final StorageUploadTask uploadTask = ref.putFile(
-      image,
-      StorageMetadata(
-        contentLanguage: 'en',
-        //customMetadata: <String, String>{'activity': 'test'},
-      ),
-    );
-
-    uploadTask.events.listen((_event) async {
-      print(_event);
-      if (_event.type == StorageTaskEventType.progress) {
-        print(
-            _event.snapshot.bytesTransferred / _event.snapshot.totalByteCount);
-        _updateCallback(_event.snapshot);
-      } else if (_event.type == StorageTaskEventType.success) {
-        print(_event.snapshot.ref);
-        downloadURL = await _event.snapshot.ref.getDownloadURL();
-
-        // upload to image collection
-        var result = await Firestore.instance.collection('Images').add({
-          'image': downloadURL,
-          'thumb': base64Image,
-          'owner_id': currentUser.uid,
-          'owner': currentUser.email,
-          'subject': "test image upload " + uniqueStr
-        });
-      } else if (_event.type == StorageTaskEventType.failure) {
-        print(_event.snapshot.error);
-      }
-    }, onDone: () async {
-      print("onDone");
-    }, onError: (_error) {
-      print(_error);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -112,7 +55,11 @@ class _HomePageState extends State<HomePage> {
             FileUploadButtonBar(
               imageFile: imageThumb,
               onUploadFile: () async {
-                uploadTheFile((_progress) {
+                ImageService().uploadTheFile({
+                  'imageThumb': imageThumb,
+                  'image': image
+                }, // information for file upload
+                    (_progress) {
                   print(_progress.bytesTransferred / _progress.totalByteCount);
                 });
               },
